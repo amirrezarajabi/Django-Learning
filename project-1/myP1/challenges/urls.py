9d5856f4867273150b3fd2056b56e3b1afd319da
amirrezarajabi <amir.reza.rajabi13800201@gmail.com>
2022-07-17 16:42:53 +0430

    Add month challenges & Dynamic path segment & Redirect

--- project-1/myP1/challenges/urls.py
+++ project-1/myP1/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge),
 ]
--- project-1/myP1/challenges/views.py
+++ project-1/myP1/challenges/views.py
@@ -1,8 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+MONTH_CHALLENGE = {
+    "january": "Be strong in january",
+    "february": "Be strong in february",
+    "march": "Be strong in march",
+    "april": "Be strong in april",
+    "may": "Be strong in may",
+    "june": "Be strong in june",
+    "july": "Be strong in july",
+    "august": "Be strong in august",
+    "september": "Be strong in september",
+    "october": "Be strong in october",
+    "november": "Be strong in november",
+    "december": "Be strong in december",
+}
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = MONTH_CHALLENGE[month]
+    except:
+        return HttpResponseNotFound("this month is not supported")
+    return HttpResponse(challenge_text)
+
+def monthly_challenge_by_number(request, month):
+    months = list(MONTH_CHALLENGE.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    
+    forward_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + forward_month)
